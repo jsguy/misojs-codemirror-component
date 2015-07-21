70533c0a3442971d067be8fe3635280ab98e29f9
jsguy <mikkel@pointful.com>
2015-07-21 17:58:00 +1000

    Add node fixes to be able to require code mirror
    Note: these could potentially interfere with other libraries - should
    really just update CodeMirror’s code instead

--- codemirror.component.js
+++ codemirror.component.js
@@ -2,20 +2,39 @@
 	Codemirror component
 */
 var m = require('mithril'),
-	//	Assume we're installed within misojs in the 
-	//	/public directory - pass in different path if needed
+	//	Assume we're installed within misojs in the "public" directory - pass in different path if needed
 	basePath = "node_modules/misojs-codemirror-component/node_modules/codemirror/";
 
+/*
+	Here we have a few fixes to make CM work in node - we only setup each,
+	if they don't already exist, otherwise we would override the browser
+*/
+global.document = global.document || {};
+global.document.createElement = global.document.createElement || function(){
+	return {
+		setAttribute: function(){}
+	};
+};
+global.window = global.window || {};
+global.window.getSelection = global.window.getSelection || function(){
+	return false;
+};
+global.navigator = global.navigator || {};
+global.navigator.userAgent = global.navigator.userAgent || "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36";
+
+//	Grab code mirror and the javascript language
+var CodeMirror = require('codemirror');
+require("codemirror/mode/javascript/javascript.js");
+
+//	Our component
 var CodemirrorComponent = {
 	//	Returns a textarea
 	view: function(ctrl, attrs) {
 		return m("div", [
-			//	Code mirror includes (it's ok to include 
-			//	these each time - browser will cache it!)
-			//	TODO: Allow more modes and config options.
-			m("SCRIPT", { src:  basePath + "lib/codemirror.js"}),
-			m("LINK", {   href: basePath + "lib/codemirror.css", rel: "stylesheet"}),
-			m("SCRIPT", { src:  basePath + "mode/javascript/javascript.js"}),
+			//	It is ok to include CSS here - the browser will cache it,
+			//	though a more ideal setup would be the ability to load only
+			//	once when required.
+			m("LINK", { href: basePath + "lib/codemirror.css", rel: "stylesheet"}),
 			m("textarea", {config: CodemirrorComponent.config(attrs)}, attrs.value())
 		]);
 	},
@@ -26,7 +45,6 @@ var CodemirrorComponent = {
 					var editor = CodeMirror.fromTextArea(element, {
 						lineNumbers: true
 					});
-
 					editor.on("change", function(instance, object) {
 						m.startComputation();
 						attrs.value(editor.doc.getValue());
